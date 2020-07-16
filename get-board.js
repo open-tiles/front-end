4cb4a2af3db520e25f9e0f9e890632cd7d287a47
Adam Blackwater <me@adamblackwater.com>
2020-07-16 10:11:16 +0100

    Return more database about each Hex

--- get-board.js
+++ get-board.js
@@ -1,37 +1,32 @@
 console.log('start')
-const URL = "http://localhost:8000/v0/board?id=2";
+const URL = "http://localhost:8000/v0/board?id=1";
 
 async function f() {
     let response = await fetch(URL);
     let data = await response.json()
     return data
  }
+
+function addHexNode(hex_id, player_id, tokens) {
+    var hexDiv = document.createElement("div");
+    hexDiv.setAttribute('class', 'hex');
+    var hexContent = document.createTextNode("Hex ID: " + hex_id + " Owner: " + player_id + " Tokens: " + tokens);
+    hexDiv.appendChild(hexContent)
+    var currentDiv = document.getElementById("board");
+    currentDiv.parentNode.insertBefore(hexDiv, currentDiv.nextSibling);
+}
+
  f()
     .then(
         function(response){
             console.log(response)
-
             var x = response.hexagons.length;
             for(i = 0; i < x; i++){
-                var newDiv = document.createElement("div");
-                newDiv.setAttribute('class', 'hex');
-                var newContent = document.createTextNode(response.hexagons[i].player_id);
-                newDiv.appendChild(newContent)
-                var currentDiv = document.getElementById("board");
-                currentDiv.parentNode.insertBefore(newDiv, currentDiv.nextSibling);
-                //document.write("playable: ", response.hexagons[i].playable)
-                //document.write("</br>")
-                //document.write("owner: ", response.hexagons[i].player_id)
-                //document.write("</br>")
-                //document.write("tokens: ", response.hexagons[i].tokens)
-                //document.write("</br>")
-                //document.write("hex-id: ", response.hexagons[i].hex_id)
-                //document.write("</br>")
-                //document.write("x: ", response.hexagons[i].x)
-                //document.write("</br>")
-                //document.write("y: ", response.hexagons[i].y)
-                //document.write("</br>")
-                //document.write("</br>")
+                addHexNode(
+                    response.hexagons[i].hex_id,
+                    response.hexagons[i].player_id,
+                    response.hexagons[i].tokens
+                )
             }
         }
     )
